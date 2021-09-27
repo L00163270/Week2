62109cf5b6583398da98ae3a5a24b6776d4f24ce
Sreejith <L00163270@student.lyit.ie>
2021-09-27 14:28:50 +0530

    Live demo for class

---
+++ structure_demo/__init__.py
@@ -0,0 +1,12 @@
+** *
+# --------------------
+# File      : .py
+# Created   : 27-09-2021 {TIME}
+# Author    : Sreejith
+# Version   : v1.0.0.
+# Licensing : (C) 2021  Sreejith , LYIT
+#             Available under  GNU Public License (GPL)
+# Description :
+# --------------------
+
+** *
